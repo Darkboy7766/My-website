d369e31888d41ce8b204bb012f4aa4350b2522db
Darkboy7766 <wpV7xS8K@eWBw76>
2026-02-05 18:35:45 +0200

    topbar changes

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -11,19 +11,19 @@ const Topbar = () => {
         <div className="hidden w-full md:flex py-2.5 px-5 font-medium text-sm text-white bg-gray-800" >
         {/* Topbar left */}
         <div className="flex w-1/2 items-center justify-start ml-10">
-            <Clock8 size={16} />
+            <Clock8 size={14} />
             <p><span className="py-1 mr-2"></span>Пн-Пт : 08-17 ч.</p>
-            <Mail  size={16} className="ml-10" />
+            <Mail  size={14} className="ml-10" />
             <p><span className="py-1 mr-2"></span>info@autogas-varna.com</p>
         </div>
         
             {/* Topbar Right */}
             <div className="flex w-1/2 items-center justify-end mr-10">
-            <Phone  size={16} />
+            <Phone  size={14} />
             <p><span className="py-1 ml-2">052 50 12 19</span></p>
-            <Smartphone size={16} className="ml-5" />
+            <Smartphone size={14} className="ml-5" />
             <p><span className="py-1 mr-2"></span>0887 67 59 81</p>
-            <Smartphone size={16} className="ml-5" />
+            <Smartphone size={14} className="ml-5" />
             <p><span className="py-1 mr-2"></span>0879 00 50 51</p>
         </div>
     </div>
