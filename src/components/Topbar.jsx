668862913fe3e57137eac59a3cef23ad7a3a64de
Darkboy7766 <wpV7xS8K@eWBw76>
2026-02-08 11:59:21 +0200

    topbar changes

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -8,22 +8,22 @@ const Topbar = () => {
 
     return (
        
-        <div className="hidden w-full md:flex py-2.5 px-5 font-medium text-sm text-white bg-gray-800" >
+        <div className="hidden w-full md:flex py-2.5 px-1 font-medium text-sm text-white bg-gray-800" >
         {/* Topbar left */}
-        <div className="flex w-1/2 items-center justify-start ml-10">
-            <Clock8 size={14} />
+        <div className="flex w-1/2 items-center justify-start ml-3 md:ml-5 xl:ml-10">
+            <Clock8 size={14} className="ml-2 md:ml-3 lg:ml-5" />
             <p><span className="py-1 mr-2"></span>Пн-Пт : 08-17 ч.</p>
-            <Mail  size={14} className="ml-10" />
+            <Mail  size={14} className="ml-2 md:ml-3 lg:ml-5" />
             <p><span className="py-1 mr-2"></span>info@autogas-varna.com</p>
         </div>
         
             {/* Topbar Right */}
-            <div className="flex w-1/2 items-center justify-end mr-10">
+            <div className="flex w-1/2 items-center justify-end mr-3 md:mr-5 lg:mr-10">
             <Phone  size={14} />
             <p><span className="py-1 ml-2">052 50 12 19</span></p>
-            <Smartphone size={14} className="ml-5" />
+            <Smartphone size={14} className="ml-2 md:ml-3 lg:ml-5" />
             <p><span className="py-1 mr-2"></span>0887 67 59 81</p>
-            <Smartphone size={14} className="ml-5" />
+            <Smartphone size={14} className="ml-2 md:ml-3 lg:ml-5" />
             <p><span className="py-1 mr-2"></span>0879 00 50 51</p>
         </div>
     </div>
