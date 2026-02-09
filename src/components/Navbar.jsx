6b59bfe5100b90c1dbdc9b425024e21dde3edb9c
Darkboy7766 <wpV7xS8K@eWBw76>
2026-02-09 19:45:17 +0200

    obfuscate emails

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
                     {navLinks.map((link, i) => (
                         <a key={i} href={link.path} className={`group flex flex-col gap-0.5 ${isScrolled ? "text-slate-800" : "text-white"}`}>
                             {link.name}
-                            <div className={`${isScrolled ? "bg-slate-800" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
+                            <div className={`${isScrolled ? "bg-slate-800" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-400`} />
                         </a>
                     ))}
                     <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-[#cd2122]' : 'text-[#FFF]'} transition-all`}>
@@ -59,12 +59,12 @@ const Navbar = () => {
 
                 {/* Mobile Menu Button */}
                 <div className="flex items-center gap-3 md:hidden">
-                    <img src={assets.menuIcon} onClick={() => setIsMenuOpen(!isMenuOpen)} alt="" className={`${isScrolled && "invert"} h-4`} />
+                    <img src={assets.menuIcon} onClick={() => setIsMenuOpen(!isMenuOpen)} alt="" className={`${isScrolled && "invert"} h-4.5`} />
                 </div>
 
                 {/* Mobile Menu */}
-                <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
-                    <button className={`${isScrolled ? "top-4" : "top-12"} absolute right-4`} onClick={() => setIsMenuOpen(false)}>
+                <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-start pt-20 gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
+                    <button className={`top-4 absolute right-4`} onClick={() => setIsMenuOpen(false)}>
                         <img src={assets.closeIcon} alt="close-menu" className="h-6.5" />
                     </button>
 
@@ -73,11 +73,6 @@ const Navbar = () => {
                             {link.name}
                         </a>
                     ))}
-
-                    <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
-                        ЗАПАЗИ ЧАС
-                    </button>
-
                 </div>
             </nav>
 
